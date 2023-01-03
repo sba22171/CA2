f564228aca8c300005654d0cc00511ffe81f2cd3
antoniodelossantos <antonio.santos@longboat.com>
2023-01-03 11:58:27 +0100

    adding tweepy_resources.py

---
+++ TweepyResources.py
@@ -0,0 +1,28 @@
+APY_KEY = "crRuaNqbQfq9DVNvroKxa1c3r"
+API_Key_Secret = "ZNwVI1WeyHI2SW791IRv0eDXUyULaxXtrvztqwzO0IEYmglNPw"
+consumer_key = "crRuaNqbQfq9DVNvroKxa1c3r"
+consumer_secret = "ZNwVI1WeyHI2SW791IRv0eDXUyULaxXtrvztqwzO0IEYmglNPw"
+access_token = "481365837-1t2frFX1RnuJQuIPaC1nVQZ72TwOTRzaQv03zrEF"
+access_token_secret = "UGJfzsPk0SB2OAvMwSIkaRMJ3apCHWiux4fSsQ3favJf0"
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+
+def get_twits_from_text(text_query)
+# text_query = '2020 US Election'
+    count = 500
+    try:
+        # Creation of query method using parameters
+        tweets = tweepy.Cursor(api.search, q=text_query).items(count)
+
+        # Pulling information from tweets iterable object
+        tweets_list = [[tweet.created_at, tweet.id, tweet.text] for tweet in tweets]
+
+        # Creation of dataframe from tweets list
+        # Add or remove columns as you remove tweet information
+        tweets_df = pd.DataFrame(tweets_list)
+
+    except BaseException as e:
+        print('failed on_status,', str(e))
+        time.sleep(3)
