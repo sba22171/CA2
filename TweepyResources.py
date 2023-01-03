0ee492503bb96435068f049c32bfa37b8920fb55
antoniodelossantos <antonio.santos@longboat.com>
2023-01-03 13:25:52 +0100

    adding tweepy_resources.py

--- TweepyResources.py
+++ TweepyResources.py
@@ -1,3 +1,4 @@
+import tweepy as tweepy
 APY_KEY = "crRuaNqbQfq9DVNvroKxa1c3r"
 API_Key_Secret = "ZNwVI1WeyHI2SW791IRv0eDXUyULaxXtrvztqwzO0IEYmglNPw"
 consumer_key = "crRuaNqbQfq9DVNvroKxa1c3r"
@@ -9,12 +10,13 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 
 
-def get_twits_from_text(text_query)
-# text_query = '2020 US Election'
+def get_twits_from_text(text_query):
     count = 500
     try:
+        print(api.verify_credentials().screen_name)
+
         # Creation of query method using parameters
-        tweets = tweepy.Cursor(api.search, q=text_query).items(count)
+        tweets = tweepy.Cursor(api.search_tweets, q=text_query).items(count)
 
         # Pulling information from tweets iterable object
         tweets_list = [[tweet.created_at, tweet.id, tweet.text] for tweet in tweets]
@@ -22,6 +24,7 @@ def get_twits_from_text(text_query)
         # Creation of dataframe from tweets list
         # Add or remove columns as you remove tweet information
         tweets_df = pd.DataFrame(tweets_list)
+        return tweets_df
 
     except BaseException as e:
         print('failed on_status,', str(e))
